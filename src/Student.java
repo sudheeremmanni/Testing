02087ad4a343e96e33779e8e9091cbf06e74ccec
sudheeremmanni <sudheer.emmanni@gmail.com>
2024-02-11 12:13:54 +0530

    simple if condition

--- src/Student.java
+++ src/Student.java
@@ -1,3 +1,11 @@
 public class Student{
     //Testing purpose
+    int a = 5;
+    int b = 6;
+    int c= 8;
+    int d = 0;
+    if (d==0 && c>=5){
+        d = a+b+c;
+        System.out.println(d);
+    }
 }
